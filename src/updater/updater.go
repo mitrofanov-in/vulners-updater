6b152337d40774dde8a53c54c6a264fa35fff8ce
Илья Митрофанов <in.mitrofanov@interfax.ru>
2022-02-09 18:23:04 +0400

    add info group about vaulners

--- src/updater/updater.go
+++ src/updater/updater.go
@@ -7,7 +7,10 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"regexp"
 )
+
+////////// STRUCT COMBINATION ///////////////
 
 type Filters struct {
 	ID           string `json:"id"`
@@ -41,19 +44,53 @@ type Plugin struct {
 	Type       string        `json:"type"`
 }
 
+////////// STRUCT SORT ////////////
+
+type FiltersSort struct {
+	ID           string `json:"id"`
+	FilterName   string `json:"filterName"`
+	Operator     string `json:"operator"`
+	Type         string `json:"type"`
+	IsPredefined bool   `json:"isPredefined"`
+	Value        string `json:"value"`
+}
+type QuerySort struct {
+	Name          string        `json:"name"`
+	Description   string        `json:"description"`
+	Context       string        `json:"context"`
+	Status        int           `json:"status"`
+	CreatedTime   int           `json:"createdTime"`
+	ModifiedTime  int           `json:"modifiedTime"`
+	Groups        []interface{} `json:"groups"`
+	Type          string        `json:"type"`
+	Tool          string        `json:"tool"`
+	SourceType    string        `json:"sourceType"`
+	StartOffset   int           `json:"startOffset"`
+	EndOffset     int           `json:"endOffset"`
+	Filters       []FiltersSort `json:"filters"`
+	SortColumn    string        `json:"sortColumn"`
+	SortDirection string        `json:"sortDirection"`
+	VulnTool      string        `json:"vulnTool"`
+}
+type PluginSort struct {
+	Query      QuerySort     `json:"query"`
+	SourceType string        `json:"sourceType"`
+	SortField  string        `json:"sortField"`
+	SortDir    string        `json:"sortDir"`
+	Columns    []interface{} `json:"columns"`
+	Type       string        `json:"type"`
+}
+
 //var strHead
 
 var url = "https://sc.interfax.ru/rest/analysis"
 
 func HttpQueryPost(url string, jstr []byte) string {
-	client := &http.Client{}
-	/*
-		client := &http.Client{
-			CheckRedirect: func(req *http.Request, via []*http.Request) error {
-				return http.ErrUseLastResponse
-			}}
-	*/
 
+	client := &http.Client{
+		CheckRedirect: func(req *http.Request, via []*http.Request) error {
+			return http.ErrUseLastResponse
+		}}
 	req_http, _ := http.NewRequest("POST", url, bytes.NewBuffer(jstr))
 	req_http.Header.Set("x-apikey", strHead)
 	resp_http, err := client.Do(req_http)
@@ -67,84 +104,92 @@ func HttpQueryPost(url string, jstr []byte) string {
 
 }
 
-var cookie_auth []*http.Cookie
+func ConvertMap(body string) interface{} {
+
+	var m map[string]interface{}
+	json.Unmarshal([]byte(body), &m)
+	f := m["response"]
+	j := f.(map[string]interface{})
+	u := j["results"]
+	return u
+}
 
 func main() {
 
+	arguments := os.Args
+	levl := arguments[1]
+	var y []interface{}
+
 	client := &http.Client{}
 
-	fmt.Println(strHead)
-
+	/////////////// TEST GET QUERY CHECK AUTHENTIFICATION //////////////
+	//fmt.Println(strHead)
 	req, err := http.NewRequest("GET", "https://sc.interfax.ru/rest/currentUser", nil)
 	if err != nil {
 		fmt.Println("Got error %s", err.Error())
 	}
-
 	req.Header.Set("x-apikey", strHead)
-
 	response, err := client.Do(req)
 	if err != nil {
 		fmt.Errorf("Got error %s", err.Error())
 	}
-
-	cookie_auth = response.Cookies()
-
-	for _, c := range cookie_auth {
-		fmt.Println(c.Name, c.Value)
-	}
-
 	defer response.Body.Close()
 
 	body, _ := ioutil.ReadAll(response.Body)
 	fmt.Println(string(body))
 
-	///////// POST QUERY /////////////
+	///////// POST QUERY CRITICAL /////////////
 
 	jStr_crit := []byte(`{"query":{"name":"","description":"","context":"","status":-1,"createdTime":0,"modifiedTime":0,"groups":[],"type":"vuln","tool":"listvuln","sourceType":"cumulative","startOffset":0,"endOffset":50,"filters":[{"id":"firstSeen","filterName":"firstSeen","operator":"=","type":"vuln","isPredefined":true,"value":"0:7"},{"id":"severity","filterName":"severity","operator":"=","type":"vuln","isPredefined":true,"value":"4"}],"vulnTool":"listvuln"},"sourceType":"cumulative","columns":[],"type":"vuln"}`)
-
-	//req_lgn.Header.Set("Content-Type", "application/json")
-
 	body_crit := HttpQueryPost(url, jStr_crit)
-	fmt.Println(string(body_crit))
+	//fmt.Println(string(body_crit))
 
-	var m map[string]interface{}
-	json.Unmarshal([]byte(body_crit), &m)
-
-	//if bodyStatus_stat == 200 {
-	f := m["response"]
-	j := f.(map[string]interface{})
-	u := j["results"]
+	u := ConvertMap(body_crit)
 
 	for _, item := range u.([]interface{}) {
 		fmt.Printf("%v", item.(map[string]interface{})["ip"])
 	}
 
-	//jStr_med := []byte(`{"query":{"name":"","description":"","context":"","status":-1,"createdTime":0,"modifiedTime":0,"groups":[],"type":"vuln","tool":"listvuln","sourceType":"cumulative","startOffset":0,"endOffset":50,"filters":[{"id":"firstSeen","filterName":"firstSeen","operator":"=","type":"vuln","isPredefined":true,"value":"0:7"},{"id":"severity","filterName":"severity","operator":"=","type":"vuln","isPredefined":true,"value":"2"}],"vulnTool":"listvuln"},"sourceType":"cumulative","columns":[],"type":"vuln"}`)
-	//jStr_med := []byte(`{"query":{"name":"","description":"","context":"","status":-1,"createdTime":0,"modifiedTime":0,"groups":[],"type":"vuln","tool":"vulndetails","sourceType":"cumulative","startOffset":0,"endOffset":50,"filters":[{"id":"uuid","filterName":"uuid","operator":"=","type":"vuln","isPredefined":true,"value":"0a35e414-39ec-49eb-9d36-b22c2735df11"},{"id":"pluginID","filterName":"pluginID","operator":"=","type":"vuln","isPredefined":true,"value":"157353"},{"id":"port","filterName":"port","operator":"=","type":"vuln","isPredefined":true,"value":"0"},{"id":"repository","filterName":"repository","operator":"=","type":"vuln","isPredefined":true,"value":[{"id":"2"}]},{"id":"firstSeen","filterName":"firstSeen","operator":"=","type":"vuln","isPredefined":true,"value":"0:7"},{"id":"severity","filterName":"severity","operator":"=","type":"vuln","isPredefined":true,"value":"2"}],"vulnTool":"vulndetails"},"sourceType":"cumulative","columns":[],"type":"vuln"}`)
-	jStr_med := []byte(`{"query":{"name":"","description":"","context":"","status":-1,"createdTime":0,"modifiedTime":0,"groups":[],"type":"vuln","tool":"sumid","sourceType":"cumulative","startOffset":0,"endOffset":50,"filters":[{"id":"firstSeen","filterName":"firstSeen","operator":"=","type":"vuln","isPredefined":true,"value":"0:7"},{"id":"severity","filterName":"severity","operator":"=","type":"vuln","isPredefined":true,"value":"2"}],"sortColumn":"severity","sortDirection":"desc","vulnTool":"sumid"},"sourceType":"cumulative","sortField":"severity","sortDir":"desc","columns":[],"type":"vuln"}`)
-	req_med, _ := http.NewRequest("POST", "https://sc.interfax.ru/rest/analysis", bytes.NewBuffer(jStr_med))
-	//req_lgn.Header.Set("Content-Type", "application/json")
-
-	req_med.Header.Set("x-apikey", strHead)
-
-	resp_med, err := client.Do(req_med)
-	if err != nil {
-		panic(err)
+	PluginSortStruct := PluginSort{
+		Query: QuerySort{
+			Name:         "",
+			Description:  "",
+			Context:      "",
+			Status:       -1,
+			CreatedTime:  0,
+			ModifiedTime: 0,
+			Groups:       y,
+			Type:         "vuln",
+			Tool:         "sumid",
+			SourceType:   "cumulative",
+			StartOffset:  0,
+			EndOffset:    50,
+			Filters: []FiltersSort{
+				{ID: "firstSeen", FilterName: "firstSeen", Operator: "=", Type: "vuln", IsPredefined: true, Value: "0:7"},
+				{ID: "severity", FilterName: "severity", Operator: "=", Type: "vuln", IsPredefined: true, Value: levl},
+			},
+			SortColumn:    "severity",
+			SortDirection: "desc",
+			VulnTool:      "sumid",
+		},
+		SourceType: "cumulative",
+		SortField:  "severity",
+		SortDir:    "desc",
+		Columns:    y,
+		Type:       "vuln",
 	}
 
-	defer resp_med.Body.Close()
-	body_med, _ := ioutil.ReadAll(resp_med.Body)
-	//fmt.Println(string(body_med))
+	jsonDataSort, _ := json.Marshal(PluginSortStruct)
 
-	var m_med map[string]interface{}
-	json.Unmarshal([]byte(body_med), &m_med)
+	jStr_medSort := []byte(jsonDataSort)
 
-	//if bodyStatus_stat == 200 {
-	f_med := m_med["response"]
-	j_med := f_med.(map[string]interface{})
-	u_med := j_med["results"]
+	///////////////////////// MUX ///////////////////////
 
-	var y []interface{}
+	//fmt.Println(string(jStr_medSort))
+
+	body_med := HttpQueryPost(url, jStr_medSort)
+
+	u_med := ConvertMap(body_med)
 
 	for _, item_med := range u_med.([]interface{}) {
 		fmt.Printf("%v ", item_med.(map[string]interface{})["pluginID"])
@@ -169,7 +214,7 @@ func main() {
 				Filters: []Filters{
 					{ID: "pluginID", FilterName: "pluginID", Operator: "=", Type: "vuln", IsPredefined: true, Value: str},
 					{ID: "firstSeen", FilterName: "firstSeen", Operator: "=", Type: "vuln", IsPredefined: true, Value: "0:7"},
-					{ID: "severity", FilterName: "severity", Operator: "=", Type: "vuln", IsPredefined: true, Value: "2"},
+					{ID: "severity", FilterName: "severity", Operator: "=", Type: "vuln", IsPredefined: true, Value: levl},
 				},
 				VulnTool: "listvuln",
 			},
@@ -185,13 +230,7 @@ func main() {
 		///////////////////////////////////// POST STRING ////////////////////////////////////
 
 		body_med := HttpQueryPost(url, jStr_med)
-		var m_med map[string]interface{}
-		json.Unmarshal([]byte(body_med), &m_med)
-		//fmt.Println(string(body_med))
-
-		f_med := m_med["response"]
-		j_med := f_med.(map[string]interface{})
-		u_med := j_med["results"]
+		u_med := ConvertMap(body_med)
 
 		for _, item := range u_med.([]interface{}) {
 			fmt.Printf("%v ", item.(map[string]interface{})["dnsName"])
@@ -205,7 +244,7 @@ func main() {
 
 			file, _ := os.Create(str + "_" + DNSFileName + ".txt")
 			file.WriteString(DNSFileName + " ")
-			file.WriteString(Uuid)
+			file.WriteString(" " + Uuid)
 
 			plugStructDetail := Plugin{
 				Query: Query{
@@ -226,7 +265,7 @@ func main() {
 						{ID: "pluginID", FilterName: "pluginID", Operator: "=", Type: "vuln", IsPredefined: true, Value: str},
 						{ID: "port", FilterName: "port", Operator: "=", Type: "vuln", IsPredefined: true, Value: "0"},
 						{ID: "firstSeen", FilterName: "firstSeen", Operator: "=", Type: "vuln", IsPredefined: true, Value: "0:7"},
-						{ID: "severity", FilterName: "severity", Operator: "=", Type: "vuln", IsPredefined: true, Value: "2"},
+						{ID: "severity", FilterName: "severity", Operator: "=", Type: "vuln", IsPredefined: true, Value: levl},
 					},
 					VulnTool: "vulndetails",
 				},
@@ -241,19 +280,20 @@ func main() {
 			//fmt.Println(string(jStr_vulndetail))
 
 			body_vulndetail := HttpQueryPost(url, jStr_vulndetail)
-			var m_vulndetail map[string]interface{}
-			json.Unmarshal([]byte(body_vulndetail), &m_vulndetail)
-			//fmt.Println(string(body_vulndetail))
-			f_vulndetail := m_vulndetail["response"]
-			j_vulndetail := f_vulndetail.(map[string]interface{})
-			u_vulndetail := j_vulndetail["results"]
+			u_vulndetail := ConvertMap(body_vulndetail)
 
 			for _, item := range u_vulndetail.([]interface{}) {
 				fmt.Printf("%v ", item.(map[string]interface{})["pluginText"])
 
 				PlugTextW := ""
 				PlugTextW = fmt.Sprintf("%v", item.(map[string]interface{})["pluginText"])
-				file.WriteString(PlugTextW)
+				reHtml := regexp.MustCompile("(?m)[\r\n]+^.*plugin_output.*$")
+				re := regexp.MustCompile("(?m)[\r\n]+^.*Installed package.*$")
+				resHtml := reHtml.ReplaceAllString(PlugTextW, "")
+				fmt.Printf(resHtml)
+				res := re.ReplaceAllString(resHtml, "")
+				fmt.Printf(res)
+				file.WriteString("" + res)
 				defer file.Close()
 			}
 
